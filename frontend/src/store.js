42af5d9713b5d080bb0885f437287b340ded1031
amosvmcr2020 <amosvmcr2020@users.noreply.github.com>
2023-02-13 19:59:01 +0000

    Prettified Code!

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,13 @@
-import { writable } from "svelte/store"
+import { writable } from "svelte/store";
 
-const stored = localStorage.getItem('current_user_id')
+const stored = localStorage.getItem("current_user_id");
 
 export const current_user_id = writable(stored || null);
 
 current_user_id.subscribe((value) => {
-    if (value === null) {
-        localStorage.removeItem("current_user_id")
-    }
-    else {
-        localStorage.current_user_id = value
-    }
-})
+  if (value === null) {
+    localStorage.removeItem("current_user_id");
+  } else {
+    localStorage.current_user_id = value;
+  }
+});
